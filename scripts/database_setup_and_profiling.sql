9a680577be0287bc4eb3cbbb9bbac32e5a9f0808
analyticgeorgy <georgeochibo13249@gmail.com>
2026-07-01 19:14:27 +0300

    Update database_setup_and_profiling.sql

--- scripts/database_setup_and_profiling.sql
+++ scripts/database_setup_and_profiling.sql
@@ -23,3 +23,92 @@ EXEC sp_help 'raw.olist_products_dataset';
 EXEC sp_help 'raw.olist_sellers_dataset';
 EXEC sp_help 'raw.product_category_name_translation';
 
+--Now we begin the data profiling
+--Stage 1 : Table Overview , Row counts
+SELECT
+COUNT(*) total_rows
+FROM raw.olist_customers_dataset;
+
+SELECT
+COUNT(*) total_rows
+FROM  raw.olist_geolocation_dataset;
+
+SELECT
+COUNT(*) total_rows
+FROM raw.olist_order_items_dataset;
+
+SELECT
+  COUNT(*) total_rows
+FROM raw.olist_order_payments_dataset;
+
+SELECT
+  COUNT(*) total_rows
+FROM raw.olist_order_reviews_dataset;
+
+SELECT
+  COUNT(*) total_rows
+FROM raw.olist_orders_dataset;
+
+SELECT
+  COUNT(*) total_rows
+FROM raw.olist_products_dataset;
+
+SELECT
+  COUNT(*) total_rows
+FROM raw.olist_sellers_dataset;
+
+SELECT
+  COUNT(*) total_rows
+FROM raw.product_category_name_translation;
+
+--Stage 2 : Missing Values
+--NOTE : Here we showcase the difference between COUNT(*) and COUNT(column_name)
+--The COUNT(*) counts all rows including the one with NULL while COUNT(column_name) counts only rows where that specific column is NOT NULL
+SELECT
+  COUNT(*) total_rows,
+  COUNT(customer_id),
+  COUNT(customer_unique_id),
+  COUNT(customer_zip_code_prefix),
+  COUNT(customer_city),
+  COUNT(customer_state)
+FROM raw.olist_customers_dataset;
+
+SELECT
+COUNT(*) total_rows,
+COUNT(geolocation_zip_code_prefix),
+COUNT(geolocation_lat),
+COUNT(geolocation_lng),
+COUNT(geolocation_city),
+COUNT(geolocation_state)
+FROM raw.olist_geolocation_dataset;
+
+SELECT
+  COUNT(*) total_rows,
+  COUNT(order_id),
+  COUNT(order_item_id),
+  COUNT(product_id),
+  COUNT(seller_id),
+  COUNT(shipping_limit_date),
+  COUNT(price),
+  COUNT(freight_value)
+FROM raw.olist_order_items_dataset;
+
+SELECT
+  COUNT(*) total_rows,
+  COUNT(order_id),
+  COUNT(payment_sequential),
+  COUNT(payment_type),
+  COUNT(payment_installments),
+  COUNT(payment_value)
+FROM raw.olist_order_payments_dataset;
+
+SELECT
+COUNT(*) total_rows,
+COUNT(review_id),
+COUNT(order_id),
+COUNT(review_score),
+COUNT(review_comment_title),
+COUNT(review_comment_message),
+COUNT(review_creation_date),
+COUNT(review_answer_timestamp)
+FROM raw.olist_order_reviews_dataset;
